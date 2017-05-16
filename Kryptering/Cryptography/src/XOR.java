ee9ff08b97e33b832e30958ffb3ee11c7cf38f90
Cenaras <mathias_rud@hotmail.com>
2017-05-16 22:36:10 +0200

    we

--- Kryptering/Cryptography/src/XOR.java
+++ Kryptering/Cryptography/src/XOR.java
@@ -1,7 +1,12 @@
 
 public class XOR {
     String xor(String binaryMessage, String binaryKey, int charCount) {
+
+        Binary binary = new Binary();
+
         String result = "";
+
+        System.out.println(binaryMessage + " " + binaryKey + " " + charCount);
 
 
         return result;
--- Kryptering/Cryptography/src/main.java
+++ Kryptering/Cryptography/src/main.java
@@ -7,6 +7,7 @@ class main {
             //Declarations of classes
             Caesar caesar = new Caesar();
             Binary binary = new Binary();
+            XOR xor = new XOR();
             Scanner myScanner = new Scanner(System.in);
 
             System.out.println("Choose which type of encryption you want: \n 1. Caesar \n 2. Binary ");
@@ -17,9 +18,9 @@ class main {
                     break;
                 case 2:
                     System.out.println("Enter the desired text as a string:");
-                    System.out.println(binary.text());
-                    //System.out.println(ctb.numberToBinary(5));
+                    System.out.println(xor.xor(binary.text(), binary.number(), binary.charNumber()));
 
+                            //System.out.println(ctb.numberToBinary(5));
                     break;
                 default:
                     System.out.println("No such option");
